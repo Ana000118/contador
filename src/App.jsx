546b65fc6723a7eb7686c9968878ad193d9aa7f8
Ana Karina Rojas Juan <rojasjuananakarina@gmail.com>
2022-04-05 11:54:31 -0500

    inserción de input y botones de suma, resta y reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,84 @@
-import {useState} from "react";
+import { useState } from "react";
+
 function App() {
-  const  [cuenta, setCuenta]=useState(0);
-  const handleClick = () => {
-   setCuenta(cuenta + 1);
-  };
+
+  const [cuenta, setCuenta] = useState(0);
+  const [paso, setPaso] = useState(0);
+
+  const handleClic = () => {
+    setCuenta(cuenta+paso);
+  }
+  const handleMenosClic = () => {
+      setCuenta(cuenta-paso);
+  }
+    const handleResetClic = () => {
+      setCuenta(cuenta - cuenta);
+      setPaso(1);
+  }
+    const handleInputChange = (event) => {
+      if (isNaN(event.target.value)){
+        return;
+      }
+      setPaso(Number(event.target.value));
+    }
 
   return (
-    <div className="App">
-     <h3>Contador</h3>
-     <hr/>
-     <h2 className="text-center">{cuenta}</h2>
-     <hr/>
-     <div style={{
-       display:"flex", 
-       justifyContent:"flex-end", 
-       marginRight:"5px"}}>
+    <div className="App">      
+        
+        <h3>Contador</h3>
+     
+        <hr />
+        <h2 className="text-center">{cuenta}</h2>
+        <hr />
 
-     <button 
-     type="button" 
-     className="btn btn-primary"
-     onClick={handleClick}>
-       +1
-       </button>
-     </div>
+        <div 
+        style={{
+        display: "flex", 
+        justifyContent: "flex-end", 
+        marginRight: "15px"
+        
+        }}>
+          
+        <label>
+          Paso
+          <input 
+          id="paso"
+          name="paso"
+          type="text" 
+          value={paso}
+          onChange={handleInputChange} 
+          style={{
+            marginLeft: "15px", 
+            width: "60px"
+            }}/>
+        </label>
+
+          <button 
+          type="button" 
+          className="btn btn-primary" 
+          onClick={handleClic}
+          style={{marginLeft:"15px"}}
+        >+1
+        </button>
+
+          <button 
+          type="button" 
+          className="btn btn-primary mx-2" 
+          onClick={handleResetClic}
+          style={{marginLeft:"15px"}}
+        >Reinicio
+        </button>
+
+            <button 
+            type="button" 
+            className="btn btn-primary mx-2" 
+            onClick={handleMenosClic}
+            style={{marginLeft:"15px"}}
+            >-1
+            </button>
+   
+        </div>
+        
     </div>
   );
 }
